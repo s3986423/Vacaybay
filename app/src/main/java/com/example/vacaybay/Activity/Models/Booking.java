c0271d16d1760eeaa68c1ccb44a1a3f674031d16
Aesitof <hnhatanh03@gmail.com>
2025-01-12 15:51:32 +0700

    Customer/Booking class

--- app/src/main/java/com/example/vacaybay/Activity/Models/Booking.java
+++ app/src/main/java/com/example/vacaybay/Activity/Models/Booking.java
@@ -1,44 +1,12 @@
 package com.example.vacaybay.Activity.Models;
 
 public class Booking {
+    private String title;
+    private String address;
+    private int bed;
     private String date;
-    private String id;
-    private String details;
-    private String payment;
-    private String checkinDate;
-    private String checkoutDate;
-
-    public Booking(String date, String id, String details, String payment, String checkinDate, String checkoutDate) {
-        this.date = date;
-        this.id = id;
-        this.details = details;
-        this.payment = payment;
-        this.checkinDate = checkinDate;
-        this.checkoutDate = checkoutDate;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public String getPayment() {
-        return payment;
-    }
-
-    public String getCheckinDate() {
-        return checkinDate;
-    }
-
-    public String getCheckoutDate() {
-        return checkoutDate;
-    }
+    private String duration;
+    private String tourGuideName;
+    private double price;
 }
 
